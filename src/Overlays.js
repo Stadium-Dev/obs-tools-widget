4ce59fdc6bc42663871c712b7e053eda429bb328
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-09-06 15:07:07 +0200

    docs

--- src/Overlays.js
+++ src/Overlays.js
@@ -1,7 +1,6 @@
 const overlays = [
     { name: "Timer Overlay", url: "../overlay/timer.html?layer-name=Timer%20Overlay&layer-width=1920&layer-height=1080" },
     { name: "Subathon Overlay", url: "../overlay/subathon.html?layer-name=Subathon%20Overlay&layer-width=1920&layer-height=1080" },
-    { name: "Labels Overlay", url: "../overlay/labels.html?layer-name=Subathon%20Overlay&layer-width=1920&layer-height=1080" }
 ];
 
 export default class Overlays {
