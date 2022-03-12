f2163277099659ea4772c7c552c6ad30d5ec00da
Tim Havlicek <tim.h4vlicek@gmail.com>
2022-03-12 16:57:54 +0100

    fix imports

--- apps/overlays/public/components/HUD.js
+++ apps/overlays/public/components/HUD.js
@@ -1,4 +1,4 @@
-import { css, html, LitElement } from '../libs/lit-element.js';
+import { css, html, LitElement } from 'lit-element';
 import Config from '../../dock/Config.js';
 
 export default class OverlayHud extends LitElement {
